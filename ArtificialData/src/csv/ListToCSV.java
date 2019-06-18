141fcd786131c14ff33de1376b866ee8f63076c3
ZZ17390 <kaeru_yuya@hotmail.com>
2019-06-18 16:38:35 +0900

    research pc commit

---
+++ ArtificialData/src/csv/ListToCSV.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author ZZ17390
+ */
+public class ListToCSV {
+    public static List<String> toList(String csv){
+        List<String> list = new ArrayList<>();
+        try(BufferedReader br = CSVFileReadWrite.readerSJIS(csv)){
+            String line;
+            while((line = br.readLine()) != null){
+                //コメント除外
+                if(line.charAt(0) == '#')
+                    continue;
+                
+                list.add(line);
+            }
+            
+            return list;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static Map<String, String> toMap(String csv, int k, int v){
+        List<String> l = toList(csv);
+        
+        Map<String, String> map = l.stream()
+                                    .map(s -> s.split(","))
+                                    .filter(s -> !s[k].equals(""))
+                                    .collect(Collectors.toMap(
+                                            s -> s[k], 
+                                            s -> s[v]
+                                    ));
+        
+        return map;
+    }
+    
+    public static void toCSV(String csv, List list){
+        try(PrintWriter pw = CSVFileReadWrite.writer(csv)){
+            list.stream().forEach(pw::println);   
+        }
+    }
+}
---
+++ ArtificialData/src/valid/MetaDataVisualize.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package valid;
+
+import csv.CSVFileReadWrite;
+import csv.ListToCSV;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author ZZ17807
+ */
+public class MetaDataVisualize {
+    public static void main(String[] args) {
+        MetaDataSet.setFiles();
+        MetaDataSet.files.keySet().stream().limit(1).forEach(f ->{
+            MetaDataDefine meta = new MetaDataDefine(MetaDataSet.files.get(f));
+            Map<String, Map<String, Double>> data = meta.getData();
+            
+            Map<String, String> layout = ListToCSV.toMap("resource\\kmresource\\layout\\"+f.replace("test", "Layout").replace(".json", ".csv"), 2, 1);
+            
+            try(PrintWriter pw = CSVFileReadWrite.writerSJIS(f+".csv")){
+                pw.println("公開キー,"+data.keySet().stream().collect(Collectors.joining(",")));
+                pw.println("非公開キー,"+data.keySet().stream().map(k -> layout.get(k.toUpperCase().split("\\.")[1])).collect(Collectors.joining(",")));
+                pw.println("データ匿名化の有無,");
+                Boolean eof = false;
+                int i = 0;
+                while(!eof){
+                    final int idx = i;
+                    String str = data.values().stream()
+                                    .map(d -> d.keySet())
+                                    .map(dk -> idx > dk.size()-1 ? "" : dk.toArray(new String[dk.size()])[idx])
+                                    .collect(Collectors.joining(","));
+                    
+                    eof = !Arrays.asList(str.split(",")).stream().filter(s -> !s.equals("")).findFirst().isPresent();
+                    
+                    pw.println(","+str);
+                    i += 1;
+                    
+                    if(i % 1000 == 0){
+                        System.out.println(i+"件 "+str);
+                        eof = true;
+                    }
+                }
+            }
+        });
+    }
+}
